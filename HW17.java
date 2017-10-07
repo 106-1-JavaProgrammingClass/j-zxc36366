29023aaffe3aed4027d1d65f364cc585dd8ab68d
zxc36366 <32056274+zxc36366@users.noreply.github.com>
2017-10-07 17:37:31 +0800

    Update HW17.java

--- HW17.java
+++ HW17.java
@@ -5,8 +5,8 @@ public class HW17 {
     public static void main(String[] args) {
         // write your code here
         Scanner scn = new Scanner(System.in);
-        int v1 = scn.nextInt();
-        System.out.println(Math.sqrt(v1)*10);
+        int a = scn.nextInt();
+        System.out.println(Math.sqrt(a)*10);
     }
 }
 
